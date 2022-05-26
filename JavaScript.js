17d547c870aabff7da3defceaf7aaa8f3802fff0
carolinact95 <74995513+carolinact95@users.noreply.github.com>
2022-05-26 09:54:14 -0500

    Add files via upload

---
+++ JavaScript.js
@@ -0,0 +1,9 @@
+function fMostrarImagen() {
+  var img = document.getElementById("imagen");
+  img.style.visibility = 'visible';
+}
+
+function fOcultarImagen() {
+  var img = document.getElementById("imagen");
+  img.style.visibility = 'hidden';
+}
